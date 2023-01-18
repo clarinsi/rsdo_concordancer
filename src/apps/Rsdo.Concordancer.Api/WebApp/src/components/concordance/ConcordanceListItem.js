691196ebf12a0ce56276a57bd126e4b6d70831c7
Rok Rejc <rok@rejc.org>
2023-01-18 13:11:08 +0100

    Don't wrap concordance results in multiple lines.

--- src/apps/Rsdo.Concordancer.Api/WebApp/src/components/concordance/ConcordanceListItem.js
+++ src/apps/Rsdo.Concordancer.Api/WebApp/src/components/concordance/ConcordanceListItem.js
@@ -6,8 +6,8 @@ const ConcordanceListItem = (props) => {
     return (
         <>
             <div className={`row ${styles.item}`} onClick={() => { props.toggleDetails(props.index) }}>
-                <div className='col text-end'><ConcordanceTokens tokens={props.item.leftContext} />&nbsp;</div>
-                <div className='col'><ConcordanceTokens tokens={[props.item.centerContext]} /> <ConcordanceTokens tokens={props.item.rightContext} /></div>
+                <div className={`col ${styles.left}`}><span><ConcordanceTokens tokens={props.item.leftContext} />&nbsp;</span></div>
+                <div className={`col ${styles.right}`}><ConcordanceTokens tokens={[props.item.centerContext]} /> <ConcordanceTokens tokens={props.item.rightContext} /></div>
             </div>
             {props.expanded && <ConcordanceDetails paragraphId={props.item.paragraphId} tokenOrder={props.item.centerContext.tokenOrder} onClose={() => { props.toggleDetails(null) }} />}
         </>
